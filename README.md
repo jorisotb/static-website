b8b4f9c53da10c6e82566a4effcfc2a5b4793cd1
JorisotB <jorisotb@gmail.com>
2022-01-22 12:13:38 +0100

    Added cmd flags

--- README.md
+++ README.md
@@ -5,37 +5,39 @@ The website is entirely static, no PHP, no JavaScript.
 
 ## Includes
 As a substitute for the include functionality, I wrote include_htm.py.
-The script crawls the website's source code looking for any of the the following statements:
+The script crawls the website's source code directory (default: /src/) looking for the the following statement:
 ```
 @include|some-file
 ```
-The 'include' statement is replaced with the contents of the corresponding file. This file should be located in the /includes directory.
+In the output directory (default: /out/), this statement is replaced with the contents of the corresponding file. This file should be located in the includes directory (default: /includes/).
 
 ## Blog Post Templates
-The tool also generates the blog posts themselves. It scans a provided template file for the following statements:
+The tool also generates the blog posts themselves. It scans a provided template file (default: /template.html) for the following statements:
 ```
 @date
 @title
 @content
 ```
-These statements are then replaced with the date, title and body of each post. Posts are stored in .htm files in the /blog directory. They must have the following format:
+These statements are then replaced with the date, title and body of each post. Posts are stored in .htm files in the blog directory (default: /blog/). They must have the following format:
 ```
 dd/mm/yyyy
 title
 content
 ```
-The resulting .html files replace these files in the destination directory.
+The resulting .html files replace these files in the output directory.
 
 ## Linking
+The generated blog posts are automatically linked usign the following statements:
 ```
 @bloglist
 @latest
 ```
-The 'bloglist' statement is replaced with a list of links to all blogposts.
+The 'bloglist' statement is replaced with a list of links to all blogposts, ordered new to old.
 The 'latest' statement is replaces with a link to the latest blogpost.
 
 Todo:
   * [x] add generation of blog posts
   * [x] add automatic linking to blog posts
-  * [ ] support for different directory structures
+  * [x] add cmd flags
+  * [x] support for different directory structures
   * [ ] support for different file types
--- include_htm.py
+++ include_htm.py
@@ -1,16 +1,18 @@
 import os
+import argparse
 
 from shutil import copyfile
-from time import time
 from datetime import datetime
-from tkinter.tix import Tree
+from time import time
 
-# Options (will probably add cmd args for these)
-src_dir   = 'src/'
-out_dir   = 'out/'
-temp_file = 'template.html'
-blog_dir  = 'blog/'
-inc_dir   = 'includes/'
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--source', type=str, default='src/', help='Source directory')
+parser.add_argument('-o', '--output', type=str, default='out/', help='Output directory')
+parser.add_argument('-b', '--blog', type=str, default='blog/', help='Blog directory')
+parser.add_argument('-i', '--include', type=str, default='includes/', help='Includes directory')
+parser.add_argument('-t', '--template', type=str, default='template.html', help='Template file')
+parser.add_argument('-c', '--clean', type=bool, default=False, help='Clean output directory before starting')
+args = parser.parse_args()
 
 blog_posts = []
 
@@ -24,25 +26,25 @@ def filepath(path, filename, target, source):
 def do_copy():
     '''Copy the source directory to the working directory.'''
     print('Copying files to destination folder...')
-    for path, dirs, files in os.walk(src_dir):
+    for path, dirs, files in os.walk(args.source):
         for file in files:
-            print('    '+filepath(path,file,out_dir,src_dir)+'...')
+            print('    '+filepath(path,file,args.output,args.source)+'...')
 
-            if not os.path.exists(filepath(path, '', out_dir, src_dir)):
-                os.makedirs(filepath(path, '', out_dir, src_dir))
+            if not os.path.exists(filepath(path, '', args.output, args.source)):
+                os.makedirs(filepath(path, '', args.output, args.source))
 
-            copyfile(filepath(path, file, src_dir, src_dir),
-                     filepath(path, file, out_dir, src_dir))
+            copyfile(filepath(path, file, args.source, args.source),
+                     filepath(path, file, args.output, args.source))
 
 def do_blog_posts():
-    '''Generate blogpost HTML files from the htm file in the blog
+    '''Generate blog post HTML files from the htm files in the blog
     directory using the template file.'''
     print('Processing blog posts...')
 
-    for path, dirs, files in os.walk(out_dir+blog_dir):
+    for path, dirs, files in os.walk(args.output+args.blog):
         for file in files:
             if file.endswith('.htm'):
-                print('    '+filepath(path,file,out_dir,out_dir)+'...')
+                print('    '+filepath(path,file,args.output,args.output)+'...')
                 post = open(path+file, 'r')
 
                 blog_posts.append([])
@@ -57,8 +59,8 @@ def do_blog_posts():
             title = post[1]
             content = ' '.join(i for i in post[2:])
 
-            src_file = open(out_dir+temp_file, 'r')
-            out_file = open(out_dir+blog_dir+title+'.html', 'a')
+            src_file = open(args.output+args.template, 'r')
+            out_file = open(args.output+args.blog+title+'.html', 'a')
             out_file.truncate(0)
 
             for line in src_file:
@@ -84,18 +86,18 @@ def do_includes(link_mode = False):
     else:
         print('Processing includes...')
 
-    for path, dirs, files in os.walk(out_dir):
+    for path, dirs, files in os.walk(args.output):
         for file in files:
             if file.endswith('.html'):
-                print('    '+filepath(path,file,out_dir,out_dir)+'...')
+                print('    '+filepath(path,file,args.output,args.output)+'...')
 
-                src_file = open(filepath(path, file, out_dir, out_dir), 'r')
-                out_file = open(filepath(path, file+'.tmp', out_dir, out_dir), 'a')
+                src_file = open(filepath(path, file, args.output, args.output), 'r')
+                out_file = open(filepath(path, file+'.tmp', args.output, args.output), 'a')
                 out_file.truncate(0)
 
                 for line in src_file:
                     if not link_mode and '@include' in line:
-                        inc_file = open(src_dir+inc_dir+line.strip().split('|')[1])
+                        inc_file = open(args.source+args.include+line.strip().split('|')[1])
                         for t_line in inc_file:
                             out_file.write(t_line)
                         inc_file.close()
@@ -110,34 +112,41 @@ def do_includes(link_mode = False):
 
                 src_file.close()
                 out_file.close()
-                os.remove(filepath(path, file, out_dir, out_dir))
-                os.rename(filepath(path, file+'.tmp', out_dir, out_dir),
-                          filepath(path, file, out_dir, out_dir))
+                os.remove(filepath(path, file, args.output, args.output))
+                os.rename(filepath(path, file+'.tmp', args.output, args.output),
+                          filepath(path, file, args.output, args.output))
 
-def do_clean():
+def do_cleanup(full_clean_mode=False):
     '''Clean htm files and empty directories from the working directory.'''
-    print('Removing unnecessary files...')
-    for path, dirs, files in os.walk(out_dir):
+    if full_clean_mode:
+        print('Emptying working directory...')
+    else:
+        print('Removing unnecessary files...')
+    for path, dirs, files in os.walk(args.output):
         for file in files:
-            if file.endswith('.htm'):
-                print('    '+filepath(path,file,out_dir,out_dir)+'...')
-                os.remove(filepath(path, file, out_dir, out_dir))
+            if full_clean_mode or file.endswith('.htm'):
+                print('    '+filepath(path,file,args.output,args.output)+'...')
+                os.remove(filepath(path, file, args.output, args.output))
 
-    print('Removing empty directories...')
-    for path, dirs, files in os.walk(out_dir):
+    if not full_clean_mode:
+        print('Removing empty directories...')
+    for path, dirs, files in os.walk(args.output):
         if not dirs and not files:
             print('    '+path+'...')
             os.rmdir(path)
 
 if __name__ == "__main__":
     start_time = time()
-    print('running include_htm with\n      source = ' + src_dir + '\n destination = ' + out_dir)
+    print('running include_htm with\n      source = ' + args.source + '\n destination = ' + args.output)
+
+    if (args.clean):
+        do_cleanup(full_clean_mode=True)
 
     do_copy()
     do_blog_posts()
     do_includes()
     do_includes(link_mode=True)
-    do_clean()
+    do_cleanup()
 
     delta_time = time() - start_time
     print('finished in '+str(delta_time)+'s')
